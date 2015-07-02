68ebf1115edc65c462b7c65075fc754b1c3e6482
Victoria Li <wingman.li@hotmail.com>
2015-07-02 13:08:05 +0800

    changed index.js register url

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ var plugins = [
 	{
 		register:require('hapi-mongodb'),
 		options: {
-			"url":"mongodb://127.0.0.1:27017/hapilovesit-project",
+			// "url":"mongodb://127.0.0.1:27017/hapilovesit-project",
+			"url":"mongodb://heroku_50ms5w2d:9j3djh3kcgeqdjg4ounnsfkt65@ds061198.mongolab.com:61198/heroku_50ms5w2d",
 			"settings":{
 				"db":{
 					"native_parser": false
